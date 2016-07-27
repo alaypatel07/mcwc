d8b561fdd250e41bc54c531bb3862b6d0c686ae2
alaypatel07 <alay1431@gmail.com>
2016-07-27 19:36:39 +0530

    Update ns2_tr_extractor to correct mistakes in calculating pdr and throughput

--- ns2_tr_extractor.py
+++ ns2_tr_extractor.py
@@ -2,7 +2,7 @@ from functools import reduce
 
 
 def get_throughput(data, destination_node, time):
-    total_bytes_received = reduce(lambda x, y: x + int(y[5]) if y[-3] == destination_node and y[0] == "r" else x, data,
+    total_bytes_received = reduce(lambda x, y: x + int(y[5]) if y[3] == destination_node[0:1] and y[0] == "r" else x, data,
                                   0)
     bits = total_bytes_received * 8
     return bits / time
@@ -10,12 +10,13 @@ def get_throughput(data, destination_node, time):
 
 def get_pdr(data, source_node, destination_node):
     link_level_source = source_node[0:1]
+    link_level_destination = destination_node[0:1]
     packets_sent = reduce(
         lambda x, y: x + 1 if y[0] == "+" and y[2] == link_level_source and y[-3] == destination_node else x,
         data, 0)
     packets_received = reduce(
-        lambda x, y: x + 1 if y[0] == "r" and y[-4] == source_node and y[-3] == destination_node else x, data, 0)
-
+        lambda x, y: x + 1 if y[0] == "r" and y[3] == link_level_destination else x, data, 0)
+    print(source_node, destination_node, packets_received, packets_sent)
     return packets_received / packets_sent
 
 
@@ -25,12 +26,12 @@ def file_extractor(file_name):
 
 
 if __name__ == '__main__':
-    file_name = "/home/alay/PycharmProjects/MC/AssignmentOut.tr"
+    file_name = "/home/alay/PycharmProjects/MC/out.tr"
     data = file_extractor(file_name)
-    n4_throughput = get_throughput(data, "4.0", 3)
+    n4_throughput = get_throughput(data, "4.0", 4.5)
     n5_throughput = get_throughput(data, "5.0", 4)
-    n4_pdr = get_pdr(data, "0.0", "5.0")
-    n5_pdr = get_pdr(data, "1.0", "4.0")
+    n5_pdr = get_pdr(data, "0.0", "5.0")
+    n4_pdr = get_pdr(data, "1.0", "4.0")
 
-    print("Throughput:-\n\tNode 4:", n4_throughput, "bps\n\tNode 5:", n5_throughput , "bps")
+    print("Throughput:-\n\tNode 4:", n4_throughput/1000, "kbps\n\tNode 5:", n5_throughput/1000, "kbps")
     print("PDR:-\n\tNode 4:", n4_pdr, "\n\tNode 5:", n5_pdr)
